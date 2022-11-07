656c10e6c9a4a0657140a6784faaea723b2ea606
Kshitij Kumar <kshitij.kumar1@office365.3pillarglobal.com>
2022-11-07 14:29:27 +0530

    changed index.js file

--- cypress/integration/Practice/demoQA.js
+++ cypress/integration/Practice/demoQA.js
@@ -14,10 +14,10 @@ describe('Fill form in Demo QA website',()=>{
         cy.get("input[id='firstName']").type('Kshitij')
         cy.get("input[id='lastName']").type('Kumar')
         cy.get("input[id='userEmail']").type('kshitij27kumar@gmail.com')
-        cy.get("input[id='gender-radio-1']").check();
+      //  cy.get("label[id='gender-radio-1']").check();
         cy.get("input[id='userNumber']").type('8529190627');
         //cy.get("input[id='dateOfBirthInput']").type('06 Nov 2022{enter}');
         cy.get("input[id='subjectsInput']").type('Data Structure and Algorithms');
-       // cy.get("textarea[id='currentAddress']").type('House No. 3, Vaishali Enclave, Civil Lines, Satna, MP',{force: true});
+        cy.get("textarea[id='currentAddress']").type('House No. 3, Vaishali Enclave, Civil Lines, Satna, MP',{force: true});
     })
 })
